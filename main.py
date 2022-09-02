a70b46d2b93eb346ee0532e0bad3dc4b921d9a6e
syzfs <6pslima2002@gmail.com>
2022-09-03 03:01:52 +0530

    Added a file for the parametric study and modified main.py accordingly

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import model
+import parametrise
 
 # Assumptions (2 refrigerants, 4 temperatures, 4 qualities, 4 component parameters):
 Hfluid = 'R1234ze(E)'
 Lfluid = 'R23'
-T1, T5 = 208, 237                      # in K
+T1, T5 = 208, 237                       # in K
 T_amb, T_gap, T_overlap = 298, 5, 5     # in K
 n_isH, n_isL = 0.75, 0.6
 Q_ref = 40000                           # in W
 Q1, Q5 = 1, 1       # (ideal saturated vapour streams)
 Q3, Q7 = 0, 0       # (ideal saturated liquid streams)
 # Thus
-# assumptions = (Hfluid, Lfluid, T1, T5, T7, T_overlap, n_isH, n_isL, Q_ref, Q1, Q3, Q5, Q7)
 assumptions = (Hfluid, Lfluid, T1, T5, T_amb, T_gap, T_overlap, n_isH, n_isL, Q_ref, Q1, Q3, Q5, Q7)
 
 # Simulation, Tabulation, Evaluation
 try:
     streams, results = model.simulate(*assumptions)
-    print('Table of Results, all in SI units (\'Q\' is dryness fraction):')
+    print('Table of Streams, all in SI units (\'Q\' is dryness fraction):')
     print(streams)
+    print('Table of Results:')
     print(results)
+    parametrise.T_amb(*assumptions)
 except:
     print('Simulation failed due to bad assumptions, try again!')
---
+++ parametrise.py
@@ -0,0 +1,18 @@
+import model
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parametrize over T_amb
+def T_amb(Hfluid, Lfluid, T1, T5, T_amb, T_gap, T_overlap, n_isH, n_isL, Q_ref, Q1, Q3, Q5, Q7):
+    T_list = np.linspace(-15,15,31) + T_amb
+    COP_list = []
+    for T in T_list:
+        COP = model.simulate(Hfluid, Lfluid, T1, T5, T, T_gap, T_overlap, n_isH, n_isL, Q_ref, Q1, Q3, Q5, Q7)[1].iloc[7,1]
+        COP_list.append(COP)
+    plt.plot(T_list-273, COP_list, '.-')
+    plt.title('Parametric Study against Ambient Temperature')
+    plt.xlabel('Ambient Temperature (in ℃)')
+    plt.ylabel('Overall COP')
+    plt.savefig('paraTamb')
+    plt.show()
+    return
